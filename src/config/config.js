0d759f516f06b76b2b649410683eac7cd78b7eb6
Henry Moland <henrymoland@gmail.com>
2018-12-26 19:47:21 -0800

    Added and imported Firebase. Created config file

---
+++ src/config/config.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore'
+import 'firebase/auth'
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAlIgIn-7EUsdn74pNA3HD2JN6Iz8Gd8Sc",
+    authDomain: "quicknote209.firebaseapp.com",
+    databaseURL: "https://quicknote209.firebaseio.com",
+    projectId: "quicknote209",
+    storageBucket: "quicknote209.appspot.com",
+    messagingSenderId: "762277026368"
+  };
+  firebase.initializeApp(config);
+
+  firebase.firestore().settings({ timestampsInSnapShots: true });
+
+  export default firebase;
